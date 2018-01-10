ae15c7b1eea5f30878f2032e8d65c4c3d618a1e0
Raghu Betina <raghu@firstdraft.com>
2018-01-10 03:57:11 +0000

    Whitelist columns in Rotation for ActiveAdmin

--- app/admin/rotation.rb
+++ app/admin/rotation.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Rotation do
 
+ permit_params :bump_group_id, :party_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
