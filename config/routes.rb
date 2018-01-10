0a9f056b4a7a58af759c1afdf261c3240bde3793
Raghu Betina <raghu@firstdraft.com>
2018-01-10 03:56:57 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "bump_groups#index"
   # Routes for the Conversation resource:
   # CREATE
