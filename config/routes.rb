6584be9d995b9bb39e7ba00b465fdb19eaf2189d
Raghu Betina <raghu@firstdraft.com>
2018-01-10 03:56:38 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "bump_groups#index"
   # Routes for the Conversation resource:
   # CREATE
   get "/conversations/new", :controller => "conversations", :action => "new"
