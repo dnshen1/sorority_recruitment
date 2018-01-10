48e8fa662d8afe760d6b9cbe99a1677655f9388f
Raghu Betina <raghu@firstdraft.com>
2018-01-10 03:56:46 +0000

    Direct association between pnms and conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,10 @@
 class Conversation < ApplicationRecord
   # Direct associations
 
+  belongs_to :ppm,
+             :class_name => "Pnm",
+             :foreign_key => "pnm_id"
+
   belongs_to :party
 
   belongs_to :member
--- app/models/pnm.rb
+++ app/models/pnm.rb
@@ -1,6 +1,9 @@
 class Pnm < ApplicationRecord
   # Direct associations
 
+  has_many   :conversations,
+             :dependent => :destroy
+
   belongs_to :party
 
   # Indirect associations
